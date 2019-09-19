459c05f4eafd31041cbd36c3e086ae295e304e2e
Gevik Babakhani <gevikb@gmail.com>
2019-09-19 16:01:29 +0200

    truncate tables befor testing

--- packages/webapi/src/tests/seed.sql
+++ packages/webapi/src/tests/seed.sql
@@ -1,1 +1,19 @@
+CREATE OR REPLACE FUNCTION truncate_tables() RETURNS void
+AS
+$$
+DECLARE
+    statements CURSOR FOR
+        SELECT tablename FROM pg_tables
+        WHERE schemaname = 'public';
+BEGIN
+    FOR stmt IN statements LOOP
+        EXECUTE 'TRUNCATE TABLE ' || quote_ident(stmt.tablename) || ' CASCADE;';
+    END LOOP;
+END;
+$$ 
+
+LANGUAGE plpgsql;
+
+select * from truncate_tables();
+
 insert into sys_role (role_name) values ('admin');
