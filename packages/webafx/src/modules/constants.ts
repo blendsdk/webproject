4f69272c842ace796efaa1ff17d024778e5907dc
Gevik Babakhani <gevikb@gmail.com>
2019-09-25 15:25:01 +0200

    Added code comments

--- packages/webafx/src/modules/constants.ts
+++ packages/webafx/src/modules/constants.ts
@@ -1,6 +1,10 @@
 import { Application } from '../application/Application';
 
+/**
+ * Type describing a module configuration
+ */
 export type TModule = (app: Application) => { id: string; module: any } | void;
+
 export const MODULE_LOGGER = "MODULE_LOGGER";
 export const MODULE_MAILER = "MODULE_MAILER";
 export const MODULE_CONFIGURATION = "MODULE_CONFIGURATION";
